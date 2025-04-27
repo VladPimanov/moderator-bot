348297be55db28a4601491118abfb0b5e27cf68a
VladPimanov <148676547+VladPimanov@users.noreply.github.com>
2025-04-27 16:34:18 +0500

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 import re
 from telegram import Update, ChatPermissions
 from telegram.ext import (
@@ -7,14 +8,22 @@ from telegram.ext import (
     ContextTypes,
     filters
 )
+from datetime import datetime
+
+
 
 TOKEN = "7902441298:AAEbXzRqmFf6xIAKVnNdkcTvqxKs7qQTBSM"
 ADMIN_IDS = []
 BANNED_WORDS = ["мат", "мат2"]
 SPAM_LIMIT = 5
+MUTE_DURATION = 10
+TIME_UPDATE_COUNT_MESSAGES = 60
+
+current_time = datetime.now()
 
 user_warnings = {}
 user_message_count = {}
+user_message_times = {}
 
 
 async def ban_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -25,6 +34,12 @@ async def ban_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.ban_chat_member(update.effective_chat.id, user_id)
     await update.message.reply_text("Пользователь забанен.")
 
+async def unmute_user(chat_id: int, user_id: int , username: str, context) -> None:
+    await asyncio.sleep(MUTE_DURATION)
+    user_message_count[user_id] = 0
+    permissions = ChatPermissions(can_send_messages=True)
+    await context.bot.restrict_chat_member(chat_id, user_id, permissions)
+    await context.bot.send_message(chat_id, f"Пользователь @{username} размучен.")
 
 async def mute_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Мьют пользователя."""
@@ -34,6 +49,8 @@ async def mute_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     permissions = ChatPermissions(can_send_messages=False)
     await context.bot.restrict_chat_member(update.effective_chat.id, user_id, permissions)
     await update.message.reply_text("Пользователь заглушен.")
+
+
 
 
 async def warn_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -51,16 +68,29 @@ async def warn_user(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 
 async def check_spam(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    global user_message_count
     """Фильтр спама и запрещённых слов."""
     user_id = update.effective_user.id
     chat_id = update.effective_chat.id
+    username = update.effective_user.username
+
+    delta_time = int(float(str(datetime.now() - current_time).split(':')[-1][1:]))
+
+    if delta_time > TIME_UPDATE_COUNT_MESSAGES:
+        user_message_count = {}
 
     user_message_count[user_id] = user_message_count.get(user_id, 0) + 1
     if user_message_count[user_id] > SPAM_LIMIT:
+
         await context.bot.delete_message(chat_id, update.message.message_id)
-        await context.bot.send_message(chat_id, f"Флуд! @{update.effective_user.username} получил мут.")
+        await context.bot.send_message(chat_id, f"Флуд! @{username} получил мут на {MUTE_DURATION} сек")
         permissions = ChatPermissions(can_send_messages=False)
         await context.bot.restrict_chat_member(chat_id, user_id, permissions)
+
+        await asyncio.create_task(
+            unmute_user(chat_id,user_id, username , context))
+
+
 
     if any(word in update.message.text.lower() for word in BANNED_WORDS):
         await context.bot.delete_message(chat_id, update.message.message_id)
